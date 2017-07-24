1435b3f837ce50ef0868d06b110452d824e96988
Yael Goodman <yagoodma@microsoft.com>
2017-07-24 18:22:43 +0300

    Added api controller for user resonse to be completed

---
+++ Controllers/TriviaGameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TriviaGame.Models;
+
+namespace TriviaGame.Controllers
+{
+    [RoutePrefix("api/TriviaGame")]
+    public class TriviaGameController : ApiController
+    {
+        [Route("UserResponse")]
+        [System.Web.Http.HttpPost]
+        public string UserResponse(UserResponseModel response)
+        {
+            //TODO
+            return "TODO";
+        }
+    }
+}
